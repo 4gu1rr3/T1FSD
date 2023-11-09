81d28b514c56ce6c80b39195bef2825b1b8a730c
Yasmin Aguirre <yas.cardozo.aguirre@gmail.com>
2023-11-08 22:53:36 -0300

    Relógio e temporizador prontos ?

--- relogio_xadrez.vhd
+++ relogio_xadrez.vhd
@@ -1,7 +1,6 @@
 --------------------------------------------------------------------------------
--- RELOGIO DE XADREZ
--- Author - Fernando Moraes - 25/out/2023
--- Revision - Iaçanã Ianiski Weber - 30/out/2023
+-- Relogio de xadrez
+-- Author - Yasmin Cardozo Aguirre 23111329
 --------------------------------------------------------------------------------
 library IEEE;
 library ieee;
@@ -10,45 +9,94 @@ use ieee.std_logic_unsigned.all;
 library work;
 
 entity relogio_xadrez is
-    port( 
-        -- COMPLETAR DE ACORDO COM A ESPECIFICACAO
+    port(clock, reset, load, j1, j2 : in std_logic;
+        init_time : in std_logic_vector(7 downto 0);
+        winJ1 : out std_logic;
+        winJ2 : out std_logic;
+        contj1 : out std_logic_vector(15 downto 0);
+        contj2 : out std_logic_vector(15 downto 0)
     );
 end relogio_xadrez;
 
 architecture relogio_xadrez of relogio_xadrez is
-    -- DECLARACAO DOS ESTADOS
-    type states is ( ...
+    -- Estados da FSM
+    type states is (INIT, DEC1, DEC2, GAN1, GAN2);
+
+    -- Sinais que armazenam o estado atual(EA) e o próximo(PE)
     signal EA, PE : states;
-    -- ADICIONE AQUI OS SINAIS INTERNOS NECESSARIOS
+
+    -- Sinais de enable para os contadores;
+    signal en1, en2: std_logic;
+
+    -- Sinais que armazeam os valores do contador1 e do contador2
+    signal cont1, cont2: std_logic_vector(15 downto 0);
     
 begin
+    -- Instancia o contador1
+    contador1 : entity work.temporizador port map (clock => clock, reset => reset, load => load, en => en1, init_time => init_time, cont => cont1);
 
-    -- INSTANCIACAO DOS CONTADORES
-    contador1 : entity work.temporizador port map ( ...
-    contador2 : entity work.temporizador port map ( ...
+    -- Instancia o contador2
+    contador2 : entity work.temporizador port map (clock => clock, reset => reset, load => load, en => en2, init_time => init_time, cont => cont2);
 
-    -- PROCESSO DE TROCA DE ESTADOS
+    -- Processo de atribuição do estado atual
     process (clock, reset)
     begin
-        
-        -- COMPLETAR COM O PROCESSO DE TROCA DE ESTADO
-
+        if reset = '1' then
+            EA <= INIT;
+        elsif rising_edge(clock) then
+            EA <= PE;
+        end if; 
     end process;
 
-    -- PROCESSO PARA DEFINIR O PROXIMO ESTADO
-    process ( ) --<<< Nao esqueca de adicionar os sinais da lista de sensitividade
+    -- Processo de troca de estados da FSM
+    process (EA, load, j1, j2, cont1, cont2)
     begin
-        case EA is
-            
-            --COMPLETAR O CASE PARA CADA UM DOS ESTADOS DA SUA MAQUINA
+        case EA is 
+            -- Estado de reset
+            when INIT =>
+                if j1 = '1' then
+                    PE <= DEC1;
+                elsif j2 = '1' then
+                    PE <= DEC2;
+                else
+                    PE <= INIT;
+                end if;
 
+            -- Estado que decrementa o contador1
+            when DEC1 =>
+                if j1 = '1' then
+                    PE <= DEC2;
+                elsif cont1 = "0000000000000000" then
+                    PE <= GAN2;
+                elsif j1 = '0' then    
+                    PE <= DEC1;
+                end if;
+
+            -- Estado que decrementa o contador2
+            when DEC2 =>
+                if j2 = '1' then
+                    PE <= DEC1;
+                elsif cont2 = "0000000000000000" then
+                    PE <= GAN1;
+                elsif j2 = '0' then 
+                    PE <= DEC2;
+                end if;
+
+            -- Estado final jogador1 é o ganhador
+            when GAN1 =>
+                PE <= INIT;
+
+            -- Estado final jogador2 é o ganhador   
+            when GAN2 =>
+                PE <= INIT;
         end case;
     end process;
 
-    
-    -- ATRIBUICAO COMBINACIONAL DOS SINAIS INTERNOS E SAIDAS - Dica: faca uma maquina de Moore, desta forma os sinais dependem apenas do estado atual!!
-    
-
+    -- atribuições de acordo com os estados
+    winJ1 <= '1' when EA = GAN1 else '0';
+    winJ2 <= '1' when EA = GAN2 else '0';
+    en1 <= '1' when EA = DEC1 else '0';
+    en2 <= '1' when EA = DEC2 else '0';
+    contj1 <= cont1;
+    contj2 <= cont2;
 end relogio_xadrez;
-
-
--- temporizador.vhd
+++ temporizador.vhd
@@ -1,34 +1,54 @@
 --------------------------------------------------------------------------------
 -- Temporizador decimal do cronometro de xadrez
--- Fernando Moraes - 25/out/2023
+-- Author - Yasmin Cardozo Aguirre 23111329
 --------------------------------------------------------------------------------
 library IEEE;
 use ieee.std_logic_1164.all;
 library work;
 
 entity temporizador is
-    port( clock, reset, load, en : in std_logic;
-          init_time : in  std_logic_vector(7 downto 0);
-          cont      : out std_logic_vector(15 downto 0)
+    port( 
+        clock, reset, load, en : in std_logic;
+        init_time : in  std_logic_vector(7 downto 0);
+        cont : out std_logic_vector(15 downto 0)
       );
 end temporizador;
 
 architecture a1 of temporizador is
+    --sinais referentes as casas do contador
     signal segL, segH, minL, minH : std_logic_vector(3 downto 0);
+
+    --sinais de enable
     signal en1, en2, en3, en4: std_logic;
+
+    --sinais referentes aos auxiliares que serão utilizados na lógica dos enables
+    signal signalSl, signalSh, signalMl, siganlMh: std_logic;
+
 begin
+    --atribuições dos sinais auxiliares de acordo com o valor das casas do cronometro
+    signalSl  <= '1' when segL = "0000" else '0';
+    signalSh <= '1' when segH = "0000" else '0';
+    signalMl <= '1' when minL = "0000" else '0';
+    siganlMh <= '1' when minH = "0000" else '0';
 
-   en1 <= ...;
-   en2 <= ...;
-   en3 <= ...;
-   en4 <= ...;
+    --atribuição dos sinais de enable de acordo com os valores dos sinais auxiliares
+    en1 <= en and not(signalSl  and signalSh and signalMl and siganlMh);
+    en2 <= en1 and signalSl; 
+    en3 <= en2 and signalSh;
+    en4 <= en3 and signalMl;
 
-   sL : entity work.dec_counter port map ( ... );
-   sH : entity work.dec_counter port map ( ... );
-   mL : entity work.dec_counter port map ( ... );
-   mH : entity work.dec_counter port map ( ... );
-   
-   cont <= minH & minL & segH & segL;
+    --Instancia do SegL
+    sL : entity work.dec_counter port map (clock => clock, reset => reset, load => load, en => en1, first_value => x"0", limit => x"9", cont => segL);
+
+    --Instancia do SegH
+    sH : entity work.dec_counter port map (clock => clock, reset => reset, load => load, en => en2, first_value => x"0", limit => x"5", cont => segH);
+
+    --Instancia do MinL
+    mL : entity work.dec_counter port map (clock => clock, reset => reset, load => load, en => en3, first_value => init_time(3 downto 0), limit => x"9", cont => minL);
+
+    --Instancia do MinH
+    mH : entity work.dec_counter port map (clock => clock, reset => reset, load => load, en => en4, first_value => init_time(7 downto 4), limit => x"9", cont => minH);
+
+    --Concatenação das casas do cronometro
+    cont <= minH & minL & segH & segL;
 end a1;
-
-
